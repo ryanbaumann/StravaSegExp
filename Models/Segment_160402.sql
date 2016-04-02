e2a00d6dc0d0e31b33ebfe7f624e5091b6b842ef
Ryan Baumann <rsbaumann@gmail.com>
2016-04-02 15:00:55 -0500

    Signed-off-by: Ryan Baumann <rsbaumann@gmail.com>

---
+++ Models/Segment_160402.sql
@@ -0,0 +1,46 @@
+﻿-- Table: "Segment"
+
+-- DROP TABLE "Segment";
+
+CREATE TABLE "Segment"
+(
+  seg_id bigint NOT NULL,
+  act_type character varying(50),
+  ath_cnt bigint,
+  cat bigint,
+  date_created timestamp without time zone,
+  distance double precision,
+  effort_cnt bigint,
+  elev_gain double precision,
+  elev_high double precision,
+  elev_low double precision,
+  name character varying(300),
+  seg_points character varying,
+  start_point geometry,
+  end_point geometry,
+  CONSTRAINT seg_id_pk PRIMARY KEY (seg_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Segment"
+  OWNER TO admin;
+
+-- Index: "seg_Name_and_props"
+
+-- DROP INDEX "seg_Name_and_props";
+
+CREATE INDEX "seg_Name_and_props"
+  ON "Segment"
+  USING btree
+  (name COLLATE pg_catalog."default", date_created, distance, cat, elev_gain, act_type COLLATE pg_catalog."default");
+
+-- Index: start_end_locs
+
+-- DROP INDEX start_end_locs;
+
+CREATE INDEX start_end_locs
+  ON "Segment"
+  USING gist
+  (start_point, end_point);
+
